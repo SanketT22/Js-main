cb1bb37914af97c222f3ab62371f6191af04e40a
SanketT22 <105072019+SanketT22@users.noreply.github.com>
2025-06-19 17:47:52 +0000

    datatypes and vairables

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";// treat all code of Js as newer version 
+// alert(3+3)  // we are usingNodeJs not Browser
+
+let name="sanket" //string
+let age= 2   //number or int
+let isLoggedIn= false //boolean
+
+/* Datatypes:-
+1.numbers
+2.bigint
+3.String = ""
+4.boolean = true/false
+5.null = empty val
+6.undefined = no any values
+7.symbol= unique
+*/
+console.log(typeof null) // o/p= object
+console.log(typeof undefined) // o/p= undefined 
---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 112233
+let accountEmail = "sanket@google.com"
+var accountPassword = "12345"
+accountCity= "pune"
+
+//accountId="35473" constant cannot be changed 
+accountEmail = "sank@.com"
+accountPassword = "88888"
+accountCity= "mumbai"
+//console.log(accountId) // basic way
+console.table([accountId,accountEmail,accountPassword,accountCity])// can print all variables in tubular format
