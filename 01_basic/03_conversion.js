fa52a3a326685bffee15882ef82cd048558e5211
SanketT22 <105072019+SanketT22@users.noreply.github.com>
2025-06-19 18:09:32 +0000

    conversions of Datatypes

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,14 @@
+let score = "33ab"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" = Number
+// "33ab" = NaN
+// true = 1
+// Null = 0
+// undefined = Nan
