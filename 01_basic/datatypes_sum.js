4603fbad245222fbb335e5acccd1bf137f362c99
SanketT22 <105072019+SanketT22@users.noreply.github.com>
2025-06-27 05:31:29 +0000

    datadtypesum

---
+++ 01_basic/datatypes_sum.js
@@ -0,0 +1,14 @@
+// Arrays
+const studs= ["sanket","ansh","tejas"];
+
+//objects 
+let Obj1={
+    name: "yash",
+    age: 22,
+}
+
+//functions
+const myFun= function(){
+    console.log("hello welcome to js")
+}
+console.log()
