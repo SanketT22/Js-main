0e803e79bcdb403686b5d633d78b19e04a7c025e
SanketT22 <105072019+SanketT22@users.noreply.github.com>
2025-06-27 05:11:02 +0000

    conversion and comparison

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -1,14 +1,26 @@
 let score = "33ab"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber= Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+//console.log(valueInNumber)
 
 // "33" = Number
 // "33ab" = NaN
 // true = 1
 // Null = 0
 // undefined = Nan
+
+/* OPERATIONS */
+
+let value= 3
+let negVal= -value
+// console.log(negVal)
+// basics
+str1="hello"
+str2=" Sanket"
+str3=str1+str2
+// console.log(str3)
+
---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,17 @@
+//console.log(2>1)
+//console.log(2>=1)
+//console.log(2<1)
+//console.log(2!=1)
+
+// PROBLEM OCCURS WHEN WE DON'T COMPARE SAME DATA TYPES
+
+// console.log("2">1)
+//                                       Automatically converts to number , but to compare 
+//                                        two different data types is not allowed
+
+
+
+// console.log(null > 0);
+// console.log(null == 0);      
+// console.log(null >= 0);
+
