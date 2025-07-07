ac0b2999337a1181bf0a0a99919c3896a1722d84
SanketT22 <105072019+SanketT22@users.noreply.github.com>
2025-07-07 17:01:16 +0000

    stringfunctions

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,12 @@
+const name1="sanket" 
+const age=21
+//console.log(name + age +"value") //it is simple and old way to concatenate 
+console.log(`my name is ${name1} and my age is ${age}`) // this way is more efficient and redable
+/* ways to define strings are 2
+   1. const name="sanket"
+   2.const name = new string("sanket")*/
+
+   const name = new String('sssanket')
+   console.log(name.toUpperCase());
+   console.log(name.charAt(2));
+   console.log(name.substring(0,5));
