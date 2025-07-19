565d34fcd3648ff892e38899ed1e66a5aef69924
SanketT22 <105072019+SanketT22@users.noreply.github.com>
2025-07-19 06:24:44 +0000

    numsandmath

--- 01_basic/05_strings.js
+++ 01_basic/05_strings.js
@@ -1,12 +1,17 @@
 const name1="sanket" 
 const age=21
 //console.log(name + age +"value") //it is simple and old way to concatenate 
-console.log(`my name is ${name1} and my age is ${age}`) // this way is more efficient and redable
+console.log(`my name is ${name1.toLocaleUpperCase()} and my age is ${age}`) // this way is more efficient and redable
 /* ways to define strings are 2
    1. const name="sanket"
    2.const name = new string("sanket")*/
 
-   const name = new String('sssanket')
+   const name = new String(' sssanket        ')
    console.log(name.toUpperCase());
    console.log(name.charAt(2));
    console.log(name.substring(0,5));
+   console.log(name.length);
+   console.log(name.slice(-8,6));
+   console.log(name.trim());
+  
+
---
+++ 01_basic/06nums_and_maths.js
@@ -0,0 +1,16 @@
+const score=100
+// console.log(score)
+
+const balance= new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString()) // converted to string
+
+const num=21.3456
+// console.log(num.toFixed(1)) // use to show number after decimal point
+
+//----------------------------maths...............................
+
+console.log(Math)
+console.log(Math.abs(-3))
+console.log(Math.sqrt(81))
